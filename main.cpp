97f51f6ad6620dd767f48d37ea57f562e68ea348
Igor Kvachenok <igor.kvachenok@prokube.ai>
2024-07-03 22:07:37 +0200

    add all info

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	// Load the data and lookup tables
-	Data::get_instance().load("new_data.txt");
+	Data::get_instance().load("nu_oph.txt");
 	Lookup::get_instance().load();
 
     DNest4::start<MyModel>(argc, argv);
--- wp12_2000/display.py
+++ wp12_2000/display.py
@@ -1,95 +1,138 @@
+from pylab import *
 import os
-
 import dnest4.classic as dn4
-import pylab
 
-pylab.rc("font", size=14, family="serif", serif="Computer Sans")
-pylab.rc("text")
+rc("font", size=14, family="serif", serif="Computer Sans")
+rc("text", usetex=False)
 
-data = pylab.loadtxt("new_data.txt")
-# truth = pylab.loadtxt("fake_data_like_nuoph.truth")
-posterior_sample = pylab.atleast_2d(dn4.my_loadtxt("posterior_sample.txt"))
+star = 'hd142'
+# star = 'hip14810'
+# star = 'corot7'
 
-width = 0.5
-pylab.hist(
-    posterior_sample[:, 1007],
-    bins=pylab.arange(0, 11) - 0.5 * width,
-    width=width,
-    color="k",
-    alpha=0.2,
-)
-pylab.xlabel("Number of Planets")
-pylab.ylabel("Number of Posterior Samples")
-pylab.xlim([-0.5, 10.5])
-pylab.show()
+data = loadtxt("../new_data.txt")
+# truth = loadtxt('fake_data_like_nuoph.truth')
+posterior_sample = atleast_2d(dn4.my_loadtxt('posterior_sample.txt'))
 
-T = posterior_sample[:, 1008:1018]
-A = posterior_sample[:, 1018:1028]
-E = posterior_sample[:, 1038:1048]
+width=0.5
+hist(posterior_sample[:,1007], bins=arange(0, 11)-0.5*width, width=width, color="k", alpha=0.2)
+xlabel('Number of Planets')
+ylabel('Number of Posterior Samples')
+xlim([-0.5, 10.5])
+get_current_fig_manager().set_window_title(os.path.dirname(os.path.realpath(__file__)))
+show()
+
+T = posterior_sample[:,1008:1018]
+A = posterior_sample[:,1018:1028]
+E = posterior_sample[:,1038:1048]
 which = T != 0
 T = T[which].flatten()
 A = A[which].flatten()
 E = E[which].flatten()
 # Trim
-# s = sort(T)
-# left, middle, right = s[0.25*len(s)], s[0.5*len(s)], s[0.75*len(s)]
-# iqr = right - left
-# s = s[logical_and(s > middle - 5*iqr, s < middle + 5*iqr)]
+#s = sort(T)
+#left, middle, right = s[0.25*len(s)], s[0.5*len(s)], s[0.75*len(s)]
+#iqr = right - left
+#s = s[logical_and(s > middle - 5*iqr, s < middle + 5*iqr)]
 
-pylab.hist(T / pylab.log(10.0), 500, alpha=0.2, color="k")
-pylab.xlabel(r"$\log_{10}$(Period/days)")
-pylab.xlim([1, 5])
-# for i in range(1008, 1008 + int(truth[1007])):
-#     pylab.axvline(truth[i] / pylab.log(10.0), color="g")
-pylab.ylabel("Number of Posterior Samples")
-pylab.show()
+hist(T/log(10.), 500, alpha=0.4, color="k")
+xlabel(r'$\log_{10}$(Period/days)')
+xlim([0, 5])
 
-pylab.subplot(2, 1, 1)
-# pylab.plot(
-#     truth[1008 : 1008 + int(truth[1007])] / pylab.log(10.0),
-#     pylab.log10(truth[1018 : 1018 + int(truth[1007])]),
-#     "ko",
-#     markersize=7,
-#     alpha=0.5,
-# )
-pylab.xlim([1, 4])
-pylab.ylim([-1, 3])
-pylab.ylabel(r"$\log_{10}$[Amplitude (m/s)$]$")
-pylab.plot(T / pylab.log(10.0), pylab.log10(A), "g.", markersize=1)
+true_periods = {
+  'hd191939': [
+    8.8803256, # hd191939
+    28.579743, # hd191939
+    38.353037, # hd191939
+    101.12, # hd191939
+    284, # hd191939
+    2200 # hd191939
+  ],
+  'hip14810': [
+    6.67, # hip 14810
+    147.7, # hip 14810
+    952, # hip 14810
+  ],
+  'corot7': [
+    0.853592, # corot-7
+    3.697, # corot-7
+    8.966 # corot-7
+  ],
+  'hd142': [
+    108.39, # d
+    349.7, # b
+    6005 # c
+  ]
+}
 
-pylab.subplot(2, 1, 2)
-# pylab.plot(
-#     truth[1008 : 1008 + int(truth[1007])] / pylab.log(10.0),
-#     truth[1038 : 1038 + int(truth[1007])],
-#     "ko",
-#     markersize=7,
-#     alpha=0.5,
-# )
-pylab.xlim([1, 5])
-pylab.xlabel(r"$\log_{10}$(Period/days)")
-pylab.ylabel("Eccentricity")
-pylab.plot(T / pylab.log(10.0), E, "g.", markersize=1)
-pylab.show()
+true_eccs = {
+  'hd191939': [
+    0.031, # hd191939
+    0.034, # hd191939
+    0.031, # hd191939
+    0.031, # hd191939
+    0.030, # hd191939
+    nan # 2200 # hd191939 # no data
+  ],
+  'hip14810': [
+    0.14399, # hip 14810
+    0.1566, # hip 14810
+    0.185, # hip 14810
+  ],
+  'corot7': [
+    0, # corot-7
+    0, # corot-7
+    0, # corot-7
+  ],
+  'hd142': [
+    0.12, # d
+    0.17, # b
+    0.21 # c
+  ]
+}
 
-data[:, 0] -= data[:, 0].min()
-t = pylab.linspace(data[:, 0].min(), data[:, 0].max(), 1000)
+if star in true_periods.keys():
+  for p in true_periods[star]:
+    axvline(log(p)/log(10.), color='g')
+ylabel('Number of Posterior Samples')
+get_current_fig_manager().set_window_title(os.path.dirname(os.path.realpath(__file__)))
+show()
 
-saveFrames = False  # For making movies
+subplot(2,1,1)
+# plot(truth[1008:1008 + int(truth[1007])]/log(10.), log10(truth[1018:1018 + int(truth[1007])]), 'ko', markersize=7, alpha=0.5)
+xlim([0, 5])
+ylim([-1, 4])
+ylabel(r'$\log_{10}$[Amplitude (m/s)$]$')
+plot(T/log(10.), log10(A), 'g.', markersize=1)
+
+subplot(2,1,2)
+plot(log(true_periods[star])/log(10.), true_eccs[star], 'ko', markersize=7, alpha=0.5)
+xlim([0, 5])
+xlabel(r'$\log_{10}$(Period/days)')
+ylabel('Eccentricity')
+plot(T/log(10.), E, 'g.', markersize=1)
+get_current_fig_manager().set_window_title(os.path.dirname(os.path.realpath(__file__)))
+show()
+
+data[:,0] -= data[:,0].min()
+t = linspace(data[:,0].min(), data[:,0].max(), 1000)
+
+saveFrames = False # For making movies
 if saveFrames:
-    os.system("rm Frames/*.png")
+  os.system('rm Frames/*.png')
 
 for i in range(0, posterior_sample.shape[0]):
-    pylab.clf()
-    # pylab.errorbar(data[:, 0], data[:, 1], fmt="k.", yerr=data[:, 2])
-    pylab.plot(t, posterior_sample[i, 0:1000], "g")
-    pylab.xlim([-0.05 * data[:, 0].max(), 1.05 * data[:, 0].max()])
-    pylab.ylim([-1.5 * max(abs(data[:, 1])), 1.5 * max(abs(data[:, 1]))])
-    # axhline(0., color='k')
-    pylab.xlabel("Time (days)", fontsize=16)
-    pylab.ylabel("Radial Velocity (m/s)", fontsize=16)
-    if saveFrames:
-        pylab.savefig("Frames/" + "%0.4d" % (i + 1) + ".png", bbox_inches="tight")
-        print("Frames/" + "%0.4d" % (i + 1) + ".png")
+  clf()
+  errorbar(data[:,0], data[:,1], fmt='k.', yerr=data[:,2])
+  plot(t, posterior_sample[i, 0:1000], 'g')
+  xlim([-0.05*data[:,0].max(), 1.05*data[:,0].max()])
+  ylim([-1.5*max(abs(data[:,1])), 1.5*max(abs(data[:,1]))])
+  #axhline(0., color='k')
+  xlabel('Time (days)', fontsize=16)
+  ylabel('Radial Velocity (m/s)', fontsize=16)
+  if saveFrames:
+    savefig('Frames/' + '%0.4d'%(i+1) + '.png', bbox_inches='tight')
+    print('Frames/' + '%0.4d'%(i+1) + '.png')
 
 
-pylab.show()
+get_current_fig_manager().set_window_title(os.path.dirname(os.path.realpath(__file__)))
+show()
